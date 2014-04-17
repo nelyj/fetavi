fcbc09069296e026c3718c45b44136049b80b10c
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-04-17 15:04:40 -0300

    inicio del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fetavi::Application.config.secret_token = '104633e8ea7b46003c9ef8a614906bae1b746981f3c88c57d67c320eb20a5ea781296426871984cb489c0cf574113171e820fad999942b2c03390fcbcb19ab88'
